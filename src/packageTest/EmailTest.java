3d7da18634314252943f74133e47bfae07c8f8e5
Guido Romero <74573750+guidorom98@users.noreply.github.com>
2024-04-12 23:44:45 -0300

    Agrega tests para validar minimo 1 numero

--- src/packageTest/EmailTest.java
+++ src/packageTest/EmailTest.java
@@ -4,8 +4,9 @@ package packageTest;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-public class EmailTest {
 
+public class EmailTest {
+	
 	@Test
 	public void testValidarCorreo_OchoDigitos_retornaTrue() {
 		
@@ -23,6 +24,20 @@ public class EmailTest {
 		int digitos = ejemploCorreo2.length();
 		
 		assertFalse(digitos>=8, "El correo tiene 8 digitos o más.");
+	}
+	
+	@Test
+	public void testValidarCorreo_ConNumero_retornaTrue() {
+		String ejemploCorreo3 = "test99@dominio.com";
+		assertTrue(ejemploCorreo3.matches(".*\\d.*"));
+		
+	}
+	
+	@Test
+	public void testValidarCorreo_ConNumero_retornaFalse() {
+		String ejemploCorreo4 = "test@dominio.com";
+		assertFalse(ejemploCorreo4.matches(".*\\d.*"));
+
 	}
 
 }
